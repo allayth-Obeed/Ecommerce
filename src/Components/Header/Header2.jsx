2b0927c6da61ffa9853c3a537f5dd555b8b18522
Layth Obeed <allayt7obeed@gmail.com>
2026-04-07 17:15:37 +0300

    finish header 2 icons

--- src/Components/Header/Header2.jsx
+++ src/Components/Header/Header2.jsx
@@ -1,66 +1,91 @@
-import { ShoppingCartOutlined } from "@mui/icons-material"
-import { Container, Stack, Typography } from "@mui/material"
-import SearchIcon from '@mui/icons-material/Search'
-import { styled, alpha } from '@mui/material/styles'
-import {InputBase} from "@mui/material"
+import { ShoppingCartOutlined } from "@mui/icons-material";
+import { Badge, Container, IconButton, Stack, Typography } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
+import { InputBase } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  backgroundColor: alpha(theme.palette.common.black, 0.6),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.35),
   },
   marginRight: theme.spacing(2),
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(3),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "white",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
   },
 }));
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    right: -3,
+    top: 6,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: "0 4px",
+  },
+}));
+
 export default function Header2() {
   return (
-    <Container sx={{display:"flex", my:3}}>
+    <Container
+      sx={{
+        display: "flex",
+        my: 3,
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
       <Stack alignItems={"center"}>
         <ShoppingCartOutlined />
         <Typography variant="body2">E-commerce</Typography>
       </Stack>
-      <Search sx = {{background:"#ede"}}>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search…"
+          inputProps={{ "aria-label": "search" }}
+        />
+      </Search>
+      <IconButton aria-label="account">
+        <PersonOutlineOutlinedIcon />
+      </IconButton>
+      <IconButton aria-label="shopping cart with badge">
+        <StyledBadge badgeContent={4} color="error">
+          <ShoppingCartIcon />
+        </StyledBadge>
+      </IconButton>
     </Container>
-  )
+  );
 }
